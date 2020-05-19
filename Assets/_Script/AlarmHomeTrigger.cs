d1357ec66d38a233d8f100d6e6fdda0018f902de
Vasyl Yevchyn <vasagaminginc@gmail.com>
2020-05-19 14:13:29 +0200

    Add home, moving script and alarm script

---
+++ Assets/_Script/AlarmHomeTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AlarmHomeTrigger : MonoBehaviour
+{
+    [SerializeField] private float _volumeSpeed;
+
+    private float _helpVolumeSpeed;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = 0;
+    }
+
+    private void Update()
+    {
+        _audioSource.volume += _helpVolumeSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Movement>(out Movement movement))
+        {
+            _helpVolumeSpeed = _volumeSpeed;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Movement>(out Movement movement))
+        {
+            _helpVolumeSpeed = -_volumeSpeed;
+        }
+    }
+}
---
+++ Assets/_Script/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            spriteRenderer.flipX = false;
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            spriteRenderer.flipX = true;
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+        }
+        
+    }
+}
